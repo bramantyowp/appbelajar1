5ca324c90169cf9e11cae4e1526789dd25df1274
Localuser <Localuser@N201002810465.toyota.co.id>
2024-09-26 10:21:10 +0700

    perbaikan controller

--- controller.js
+++ controller.js
@@ -3,6 +3,6 @@ var response = require('./res');
 var connetion = require('./koneksi');
 
 exports.index = function(req,res) {
-    response.ok( "Aplikasi rest api jalan lhoo..");
+    response.ok( "Aplikasi rest api jalan lhoo..", res);
 
 }
