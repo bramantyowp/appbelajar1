af45caed366145a3cbd49ba2cb1f3af90aa74897
Localuser <Localuser@N201002810465.toyota.co.id>
2024-09-26 09:58:30 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+var routes =require('./routers');
+const routers = require('./routers');
+routers(app);
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
